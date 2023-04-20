25ac668a5f929f98305ed2f2c3a352b4fabfaae2
Vadim <DJ_ZeR@mail.ru>
2023-04-20 16:06:03 +0300

    Задание выполнено без перегрузки оператора

---
+++ Chapter 14/SecondTack/wine.h
@@ -0,0 +1,84 @@
+#ifndef WINE_H_
+#define WINE_H_
+#include<iostream>
+#include<valarray>
+template<class T1,class T2>
+class Pair
+{
+private:
+    T1 a; //valarray<int> a
+    T2 b;
+public:
+    Pair(){}
+    T1 &first();
+    T2 &second();
+    T1 first()const{return a;}
+    T2 second()const{return b;}
+    Pair(const T1 &a,const T2 &b):a(a),b(b){}
+    // T1 &operator=(const &Pair pa)
+};
+template<class T1,class T2>
+T1 &Pair<T1,T2>::first()
+{
+    return a;
+}
+template<class T1,class T2>
+T2 &Pair<T1,T2>::second()
+{
+    return b;
+}
+// template<class T1,class T2>
+// T1 &Pair<T1,T2>::operator=(const &Pair pa)
+// {
+
+// }
+typedef std::valarray<int> ArrayInt;
+typedef Pair<ArrayInt,ArrayInt> PairArray;
+// PairArray::operator=(PairArray(ArrayInt(),ArrayInt()))
+// {
+
+// }
+class wine:private std::string, private PairArray
+{
+private:
+    int years;
+public:
+    wine():std::string("None"),years(0),PairArray(){}
+    wine(const char *l, int y,const int yr[],const int bot[]):std::string(l),years(y),PairArray(ArrayInt(yr,y),ArrayInt(bot,y)){}
+    wine(const char *l, int y):std::string(l),years(y),PairArray(ArrayInt(y),ArrayInt(y)){}
+    void GetBottles();
+    std::string &Label();
+    int sum();
+    void Show();
+};
+void wine::GetBottles()
+{
+    using namespace std;
+    cout<<"Enter "<<Label()<<" data for "<<years<<" years(s):"<<endl;
+    for (int i = 0; i < years; i++)
+    {
+        cout<<"Enter year: ";
+        cin>>PairArray::first()[i];
+        cout<<"Enter bottles for that year: ";
+        cin>>PairArray::second()[i];
+    } 
+}
+std::string &wine::Label()
+{
+    return (std::string &)*this;
+}
+int wine::sum()
+{
+    return PairArray::second().sum();
+}
+void wine::Show()
+{
+    using namespace std;
+    cout<<"Wine: "<<string(*this)<<endl;
+    cout<<"     Year     Bottles"<<endl;
+    for (int i = 0; i < years; i++)
+    {
+        cout<<"     "<<PairArray::first()[i]<<"     "<<PairArray::second()[i]<<endl;
+    }    
+}
+#endif
---
+++ Chapter 14/SecondTack/wine_main.cpp
@@ -0,0 +1,23 @@
+#include"wine.h"
+
+int main()
+{
+    using namespace std;
+    cout<<"Enter name of wine: ";
+    char lab[50];
+    cin.getline(lab,50);
+    cout<<"Enter numbers of years: ";
+    int yrs;
+    cin>>yrs;
+    wine holding(lab,yrs);
+    holding.GetBottles();
+    holding.Show();
+    const int YRS =3;
+    int y[YRS]={1993,1995,1998};
+    int b[YRS]={48,60,72};
+    wine more("Gushing Grape Red",YRS,y,b);
+    more.Show();
+    cout<<"total bottles for "<<more.Label()<<": "<<more.sum()<<endl;
+    cout<<"Bye\n";
+    return 0;
+}
